f2ae684e8a3bd480e48f2b50f4f639bec7f1f5ed
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-05-18 10:22:15 -0700

    benchmark_result is provided by Jute now

--- test/TestUtils.jl
+++ test/TestUtils.jl
@@ -1,17 +1,3 @@
-function benchmark_result(trial)
-    time_str = BenchmarkTools.prettytime(minimum(trial.times))
-
-    if trial.allocs > 0
-        mem_str = BenchmarkTools.prettymemory(trial.memory)
-        alloc_str = ", $mem_str ($(trial.allocs) allocs)"
-    else
-        alloc_str = ""
-    end
-
-    time_str * alloc_str
-end
-
-
 function ref_mul(p, y)
     res = p
     for i in 2:y
